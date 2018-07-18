2b29994f115c08875f260e7d4483d774413de8fe
Konstantin Baierer <unixprog@gmail.com>
2018-07-18 18:16:24 +0200

    more flexible, document some

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ export workspace_dir="/tmp/test-ocrd-olena-binarize"
 # Init workspace
 rm -rf "$workspace_dir"
 ocrd workspace init "$workspace_dir"
-ocrd workspace -d "$workspace_dir" add -G OCR-D-IMG -i orig -m image/png "$assets/scribo-test/orig.png"
+ocrd workspace -d "$workspace_dir" add -G OCR-D-IMG -i orig -m image/tiff "$assets/scribo-test/orig.tiff"
 
 declare -a algos=(sauvola sauvola-ms-fg sauvola-ms sauvola-ms-split)
 for algo in "${algos[@]}";do
@@ -20,7 +20,7 @@ done
 
 for algo in "${algos[@]}";do
     echo >&2 "# Diffing $algo"
-    should=$(wc -c "$workspace_dir"/*/*$algo.png | grep -o '^[0-9]*')
+    should=$(wc -c "$workspace_dir"/OCR-D-IMG-BIN/*$algo.png | grep -o '^[0-9]*')
     actual=$(wc -c "$assets/scribo-test/orig.${algo}.png" | grep -o '^[0-9]*')
     if [[ $should != $actual ]];then
         echo "not ok - $algo: Expected $should but is $actual"
